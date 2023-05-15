87d2f5dec95f853309151147ecf9d74e4e5f597e
42neshka <burenin_alexander@list.ru>
2023-05-15 12:03:21 +0400

    Dictionary

--- milk = False.py
+++ milk = False.py
@@ -1,5 +1,17 @@
-milk = True
-if milk:
-	print('ebal t9')
-	
-print(123)
+milk = 1
+coffee = 1
+curd = True
+sour_cream = True
+banana = True
+
+
+def breakfast_def():
+	for breakfast in range(5):
+		if milk == 1:
+			if milk and coffee == 1:
+					print('У тебя на завтрак - Капучино')
+					milk = milk - 1
+		elif curd:
+			print('Prog dont working')
+
+breakfast_def()
