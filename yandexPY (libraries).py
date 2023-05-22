c962efed34aedae68007ebf18498dc5caf2b0377
Alexander Burenin <burenin_alexander@list.ru>
2023-05-22 15:34:52 +0400

    library

---
+++ yandexPY (libraries).py
@@ -0,0 +1,21 @@
+""" #todo Список библиотек
+#! datetime as dt
+
+#? function:
+.timedelta() - промежуток
+time_bottas = dt.timedelta(minutes=1, seconds=25, microseconds=273250)
+
+#? Методы 
+.utcnow()	#*возращает текущий момент времени
+utc_time = dt.datetime.utcnow()
+.strftime() #*выводит время в желаемом формате
+f_time = city_time.strftime("%H:%M")
+
+
+#! random as r
+
+#? function:
+choice() #* рандомит элементы списка
+randint() #* рандомит числа
+
+"""
