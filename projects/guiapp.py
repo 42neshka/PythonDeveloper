a0d0519b40e77c97bb44229362edb1d4e265e30f
Alexander Burenin <burenin_alexander@list.ru>
2023-08-08 16:18:08 +0400

    nt

--- projects/guiapp.py
+++ projects/guiapp.py
@@ -1,6 +1,26 @@
 import tkinter as tk
 
 win = tk.Tk()
-# win.title('Gui_Application')
+win.title('love Adeline')
+win.geometry('500x500+500+250')
+win.minsize(250, 250)
+win.maxsize(750, 750)
+win.resizable(True, True)
+# win.config(bg='grey')
+
+text1 = tk.Label(win, text='''Hello, 
+Dear user!''',
+				bg='grey',
+				fg='white',
+				font=('Arial', 15, 'bold'),
+				pady=15,
+				width=20,
+				height=2,
+				justify=tk.CENTER,
+				anchor='n',
+				relief=tk.RAISED,
+				bd=5
+				)
+text1.pack()
 
 win.mainloop()
